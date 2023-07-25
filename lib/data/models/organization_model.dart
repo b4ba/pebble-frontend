e045681718897c6233ceb740250f4650166e37d9
Alex Babalitis <alexandros.babalitis@gmail.com>
2023-07-25 17:53:46 +0100

    Update organization_model.dart

--- lib/data/models/organization_model.dart
+++ lib/data/models/organization_model.dart
@@ -7,12 +7,32 @@ class Organization {
   final String description;
   final IconData icon;
 
-  const Organization({required this.id, required this.name, required this.description, required this.icon});
+  const Organization(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required this.icon});
 
   static List<Organization> organizations = [
-    const Organization(id: '0', name: 'Edinburgh Baking Society', description: 'Description here', icon: Icons.cake),
-    const Organization(id: '1', name: 'Edinburgh University Sports Union (EUSU)', description: 'Description here', icon: Icons.sports_baseball_rounded),
-    const Organization(id: '2', name: 'Informatics 19/23', description: 'Description here', icon: Icons.computer_rounded),
-    const Organization(id: '3', name: 'Edinburgh University Students\' Association (EUSA)', description: 'Description here', icon: Icons.people),
+    const Organization(
+        id: '0',
+        name: 'Edinburgh Baking Society',
+        description: 'Description here',
+        icon: Icons.cake),
+    const Organization(
+        id: '1',
+        name: 'Edinburgh University Sports Union (EUSU)',
+        description: 'Description here',
+        icon: Icons.sports_baseball_rounded),
+    const Organization(
+        id: '2',
+        name: 'Informatics 19/23',
+        description: 'Description here',
+        icon: Icons.computer_rounded),
+    const Organization(
+        id: '3',
+        name: 'Edinburgh University Students\' Association (EUSA)',
+        description: 'Description here',
+        icon: Icons.people),
   ];
 }
